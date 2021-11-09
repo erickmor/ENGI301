5ec9dd2118dd510dbb7fe9296ea6d1b8a7127224
Demo User <debian@beaglebone.localdomain>
2021-11-09 06:41:27 +0000

    Updated hardware testing

--- project_01/mainproject/code/projectcompontentbackboard.py
+++ project_01/mainproject/code/projectcompontentbackboard.py
@@ -31,6 +31,8 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 --------------------------------------------------------------------------
 
+Code used to test out components and physical hardware connections.
+
 
 """
 
